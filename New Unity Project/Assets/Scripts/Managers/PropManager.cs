b0ed282ab423f2646ab19efaa5a1e145e55d92d9
unknown <ce.brun@J001-0128-04.etudiant.rubika-edu.com.local>
2015-12-17 11:21:57 +0100

    CreateProject

---
+++ New Unity Project/Assets/Scripts/Managers/PropManager.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PropManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
